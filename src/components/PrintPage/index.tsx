2fe28531217c19281769f80e25b3d745b2caa445
Afwan-Pratama <afwan.pratama@protonmail.com>
2025-09-21 13:32:51 +0700

    fix: fixing types

--- src/components/PrintPage/index.tsx
+++ src/components/PrintPage/index.tsx
@@ -4,7 +4,48 @@ import Database from "@tauri-apps/plugin-sql";
 import { useEffect, useRef, useState } from "react";
 import { useReactToPrint } from "react-to-print"
 
-export default function PrintPage(props) {
+interface pembelianProps {
+  id: number
+  wilayah_id: number
+  kode_id: number
+  nama: string
+  harga: number
+  bruto: number
+  netto: number
+  jumlah_harga: number
+}
+
+interface biayaPembelianProps {
+  administrasi: number
+  pajak: number
+  rokok: number
+  mod_rokok: number
+}
+
+interface biayaAkhirProps {
+  administrasi: number
+  pajak: number
+  rokok: number
+  jumlah_rokok: number
+}
+
+interface jumlahTotalProps {
+  harga: number
+  bruto: number
+  netto: number
+}
+
+type PrintPageProps = {
+  dataPembelian: pembelianProps[]
+  dataBiaya: biayaPembelianProps | undefined
+  biayaAkhir: biayaAkhirProps
+  jumlahTotal: jumlahTotalProps
+  totalAkhir: number
+  handleOnlySave: () => void
+  handleClose: () => void
+}
+
+export default function PrintPage(props: PrintPageProps) {
 
   const { dataPembelian, dataBiaya, biayaAkhir, jumlahTotal, totalAkhir, handleOnlySave, handleClose } = props
 
@@ -18,12 +59,12 @@ export default function PrintPage(props) {
   async function handlePrint() {
     reactToPrint()
     handleClose()
-    await handleOnlySave()
+    handleOnlySave()
   }
 
   async function getData() {
     const db = await Database.load('sqlite:test.db')
-    const getKode = await db.select('SELECT kode from Kode WHERE id = $1', [dataPembelian[0].kode_id])
+    const getKode: { kode: string }[] = await db.select('SELECT kode from Kode WHERE id = $1', [dataPembelian[0].kode_id])
     setKode(getKode[0].kode)
   }
 
@@ -72,15 +113,15 @@ export default function PrintPage(props) {
                 <Typography variant="body2">JUMLAH KERANJANG : {dataPembelian.length}</Typography>
               </Stack>
               <Stack direction='row' justifyContent='space-between'>
-                <Typography variant="body2">ADMINISTRASI     : Rp.{dataBiaya.administrasi}</Typography>
+                <Typography variant="body2">ADMINISTRASI     : Rp.{dataBiaya?.administrasi}</Typography>
                 <Typography variant="body2">Rp. {biayaAkhir.administrasi}</Typography>
               </Stack>
               <Stack direction='row' justifyContent='space-between'>
-                <Typography variant="body2">PAJAK : {dataBiaya.pajak}%</Typography>
+                <Typography variant="body2">PAJAK : {dataBiaya?.pajak}%</Typography>
                 <Typography variant="body2">Rp. {biayaAkhir.pajak}</Typography>
               </Stack>
               <Stack direction='row' justifyContent='space-between'>
-                <Typography variant="body2">ROKOK : Rp. {dataBiaya.rokok}</Typography>
+                <Typography variant="body2">ROKOK : Rp. {dataBiaya?.rokok}</Typography>
                 <Typography variant="body2">x {biayaAkhir.jumlah_rokok}</Typography>
                 <Typography variant="body2">Rp. {biayaAkhir.rokok}</Typography>
               </Stack>
